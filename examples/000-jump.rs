a79be5fade67b98760cba308d43c87f9fd2fb72e
Maxime Buffa <mbuffa@users.noreply.github.com>
2025-03-20 15:02:46 +0100

    Fix 000-Jump by using a proper equation

--- examples/000-jump.rs
+++ examples/000-jump.rs
@@ -2,6 +2,8 @@
 
 use bevy::prelude::*;
 
+const SCREEN_UNIT: f32 = 22.0;
+
 enum JumpingStates {
     Idle,
     Airborne,
@@ -21,9 +23,14 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
-        .add_systems(Update, handle_jumping_state)
-        .add_systems(Update, update_player_velocity)
-        .add_systems(Update, update_player_transform_and_handle_ground_collision)
+        .add_systems(
+            Update,
+            (
+                handle_jumping_state,
+                update_player_velocity,
+                update_player_transform,
+            ),
+        )
         .run();
 }
 
@@ -33,9 +40,6 @@ fn setup(
     mut materials: ResMut<Assets<ColorMaterial>>,
     window: Query<&Window>,
 ) {
-    let window = window.single();
-    let width = window.resolution.width();
-
     commands.spawn(Camera2d);
 
     commands.spawn((
@@ -47,7 +51,7 @@ fn setup(
         },
         Mesh2d(meshes.add(Rectangle::new(100.0, 100.0))),
         MeshMaterial2d(materials.add(Color::srgb(1.0, 1.0, 1.0))),
-        Transform::from_xyz(-width / 2.0 + 50.0, 0.0, 0.0),
+        Transform::from_xyz(0.0, 0.0, 0.0),
     ));
 }
 
@@ -56,55 +60,62 @@ fn handle_jumping_state(
     keyboard: Res<ButtonInput<KeyCode>>,
     time: Res<Time>,
 ) {
-    if keyboard.just_pressed(KeyCode::Space) {
-        for mut jumping_state in &mut query {
-            match jumping_state.state {
-                JumpingStates::Idle => {
-                    // println!("jump!");
-                    jumping_state.state = JumpingStates::Airborne;
-                    jumping_state.jump_started_at = time.elapsed_secs();
-                }
-                _ => {}
-            }
-        }
+    if query.is_empty() {
+        return;
     }
-}
 
-fn update_player_velocity(mut query: Query<&mut JumpingState, With<Player>>, time: Res<Time>) {
-    let tt = time.elapsed_secs();
+    if keyboard.just_pressed(KeyCode::Space) {
+        let mut jumping_state = query.single_mut();
 
-    for mut jumping_state in &mut query {
         match jumping_state.state {
-            JumpingStates::Airborne => {
-                // println!("airborne!");
-                let x = tt - jumping_state.jump_started_at;
-
-                let ax2 = -50.0 * (x - 0.25).powi(2);
-                let bx = 2.0 * x;
-                let c = 3.0 - x;
-
-                jumping_state.current_velocity = ax2 + bx + c;
+            JumpingStates::Idle => {
+                jumping_state.state = JumpingStates::Airborne;
+                jumping_state.jump_started_at = time.elapsed_secs();
             }
-
             _ => {}
         }
     }
 }
 
-fn update_player_transform_and_handle_ground_collision(
-    mut query: Query<(&mut Transform, &mut JumpingState), With<Player>>,
-) {
-    for (mut transform, mut jumping_state) in &mut query {
-        // Touching the ground; we reset velocity and state.
-        if transform.translation.y < 0.0 {
-            jumping_state.current_velocity = 0.0;
-            jumping_state.state = JumpingStates::Idle;
-            jumping_state.jump_started_at = 0.0;
-            transform.translation.y = 0.0;
+fn update_player_velocity(mut query: Query<&mut JumpingState, With<Player>>, time: Res<Time>) {
+    if query.is_empty() {
+        return;
+    }
+
+    let tt = time.elapsed_secs();
+
+    let mut jumping_state = query.single_mut();
+
+    if jumping_state.current_velocity < 0.0 {
+        jumping_state.state = JumpingStates::Idle;
+    }
+
+    match jumping_state.state {
+        JumpingStates::Airborne => {
+            let x: f32 = tt - jumping_state.jump_started_at;
+            // h\ +\ v\cdot x-\frac{1}{2}\cdot g\cdot x^{2}
+            // h + v * x - 1/2 g * x²
+            let y: f32 = 0.0 + (70.0 * x) - 0.5 * 160.0 * x.powi(2);
+            jumping_state.current_velocity = y;
         }
 
-        transform.translation.y = jumping_state.current_velocity * 100.0;
+        _ => {
+            jumping_state.current_velocity = 0.0;
+            jumping_state.jump_started_at = 0.0;
+        }
+    }
+}
 
-        // println!("y = {:?}", transform.translation.y);
+fn update_player_transform(mut query: Query<(&mut Transform, &JumpingState), With<Player>>) {
+    if query.is_empty() {
+        return;
+    }
+
+    let (mut transform, jumping_state) = query.single_mut();
+
+    if transform.translation.y < 0.0 {
+        transform.translation.y = 0.0;
+    } else {
+        transform.translation.y = jumping_state.current_velocity * SCREEN_UNIT;
     }
 }
