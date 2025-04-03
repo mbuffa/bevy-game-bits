fccbf294f0e24bf7554dfb3d3c4d7744fd126c6b
Maxime Buffa <mbuffa@users.noreply.github.com>
2025-04-03 23:25:23 +0200

    Rework obstacles spawn, add some instructions and streamline a few things

--- examples/004-infinite-runner.rs
+++ examples/004-infinite-runner.rs
@@ -1,7 +1,6 @@
 // Features an Infinite Runner prototype
 
 // TODO:
-// * Add instructions on the InsertCOin state.
 // * Check for a better way to handle state transitions
 // * Check for a better way to handle entities spawn
 // * Declare a Jump or Player Plugin
@@ -12,13 +11,16 @@
 use bevy::math::bounding::{Aabb2d, IntersectsVolume};
 use bevy::prelude::*;
 
+const SCREEN_UNIT: f32 = 10.0;
 const WINDOW_WIDTH: f32 = 800.0;
 const WINDOW_HEIGHT: f32 = 600.0;
-const SCREEN_UNIT: f32 = 22.0;
-const PLAYER_WIDTH: f32 = 24.0;
-const PLAYER_HEIGHT: f32 = 72.0;
+const HORIZON_HEIGHT: f32 = 32.0;
+const PLAYER_WIDTH: f32 = 32.0;
+const PLAYER_HEIGHT: f32 = 32.0;
+const OBSTACLE_WIDTH: f32 = 8.0;
+const OBSTACLE_HEIGHT: f32 = 72.0;
 const SCOREBOARD_TEXT_PADDING: Val = Val::Px(4.0);
-const SCOREBOARD_FONT_SIZE: f32 = 16.0;
+const SCOREBOARD_FONT_SIZE: f32 = 48.0;
 
 #[derive(Resource)]
 struct WindowSize(f32, f32);
@@ -45,6 +47,11 @@ struct CollisionEvent;
 #[derive(Resource)]
 struct Score(u32);
 
+#[derive(Resource)]
+struct SpawnCooldowns {
+    tier1: f32,
+}
+
 #[derive(Component)]
 struct ScoreText;
 
@@ -69,19 +76,27 @@ struct Obstacle;
 #[derive(Component)]
 struct GameOverText;
 
+#[derive(Component)]
+struct HitSpaceText;
+
+#[derive(Component)]
+struct InstructionsText;
+
 fn main() {
     App::new()
         .insert_resource(WindowSize(WINDOW_WIDTH, WINDOW_HEIGHT))
         .insert_resource(ClearColor(Color::srgb(0.0, 0.0, 0.0)))
         .insert_resource(GameState(GameStates::InsertCoin))
         .insert_resource(Score(0))
+        .insert_resource(SpawnCooldowns { tier1: 0.0 })
         .add_event::<GameStateEvent>()
         .add_event::<CollisionEvent>()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, (setup, spawn_obstacles))
+        .add_systems(Startup, setup)
         .add_systems(
             Update,
-            maybe_transit_to_play_state.run_if(resource_equals(GameState(GameStates::InsertCoin))),
+            (maybe_transit_to_play_state, maybe_hide_instructions_text)
+                .run_if(resource_equals(GameState(GameStates::InsertCoin))),
         )
         .add_systems(
             Update,
@@ -95,8 +110,9 @@ fn main() {
         .add_systems(
             Update,
             (
-                spawn_entities,
-                hide_game_over_text,
+                maybe_hide_instructions_text,
+                spawn_player,
+                spawn_obstacles,
                 move_obstacles,
                 update_score_text,
                 detect_collisions,
@@ -129,9 +145,11 @@ fn setup(
     // Camera
     commands.spawn(Camera2d);
 
-    // Ground
+    add_instructions_text(&mut commands, &window);
+
+    // Horiwon
     commands.spawn((
-        Transform::from_xyz(0.0, 0.0 - (PLAYER_HEIGHT / 2.0), 0.0),
+        Transform::from_xyz(0.0, HORIZON_HEIGHT, -1.0),
         Mesh2d(meshes.add(Rectangle::new(window_size.0, 1.0))),
         MeshMaterial2d(materials.add(Color::srgb(1.0, 1.0, 1.0))),
     ));
@@ -154,7 +172,7 @@ fn setup(
     ));
 }
 
-fn spawn_entities(
+fn spawn_player(
     mut commands: Commands,
     window_size: Res<WindowSize>,
     mut events: EventReader<GameStateEvent>,
@@ -201,6 +219,36 @@ fn spawn_entities(
     }
 }
 
+fn spawn_obstacles(
+    mut commands: Commands,
+    window_size: Res<WindowSize>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    time: Res<Time>,
+    mut cooldowns: ResMut<SpawnCooldowns>,
+) {
+    cooldowns.tier1 -= time.delta().as_secs_f32();
+
+    if cooldowns.tier1 < 0.0 {
+        cooldowns.tier1 = 2.0;
+
+        commands.spawn((
+            Obstacle,
+            Transform {
+                translation: Vec3::new(
+                    window_size.0 * 2.,
+                    (OBSTACLE_HEIGHT - PLAYER_HEIGHT) / 2.,
+                    0.0,
+                ),
+                scale: Vec3::new(OBSTACLE_WIDTH, OBSTACLE_HEIGHT, 1.),
+                ..Default::default()
+            },
+            Mesh2d(meshes.add(Rectangle::new(1., 1.))),
+            MeshMaterial2d(materials.add(Color::srgb(1.0, 1.0, 1.0))),
+        ));
+    }
+}
+
 fn despawn_entities(
     mut commands: Commands,
     query: Query<Entity, Or<(With<Player>, With<Obstacle>)>>,
@@ -210,29 +258,16 @@ fn despawn_entities(
     }
 }
 
-fn spawn_obstacles(
+fn move_obstacles(
     mut commands: Commands,
     window_size: Res<WindowSize>,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-) {
-    commands.spawn((
-        Obstacle,
-        Transform::from_xyz(window_size.0 * 2., 0.0, 0.0),
-        Mesh2d(meshes.add(Rectangle::new(PLAYER_WIDTH, PLAYER_HEIGHT))),
-        MeshMaterial2d(materials.add(Color::srgb(0.7, 0.0, 0.0))),
-    ));
-}
-
-fn move_obstacles(
-    window_size: Res<WindowSize>,
-    mut obstacles: Query<&mut Transform, With<Obstacle>>,
+    mut obstacles: Query<(Entity, &mut Transform), With<Obstacle>>,
     mut score: ResMut<Score>,
 ) {
-    for mut transform in obstacles.iter_mut() {
-        if transform.translation.x < (-window_size.0 / 2.) {
-            transform.translation.x = window_size.0 * 2.;
+    for (entity, mut transform) in obstacles.iter_mut() {
+        if transform.translation.x < (-window_size.0 / 2.) - OBSTACLE_WIDTH / 2. {
             score.0 += 100;
+            commands.entity(entity).despawn();
         } else {
             transform.translation.x -= 10.0;
         }
@@ -256,8 +291,10 @@ fn detect_collisions(
     );
 
     for transform in obstacles.iter() {
-        let obstacle_bounding_box =
-            Aabb2d::new(transform.translation.truncate(), transform.scale.truncate());
+        let obstacle_bounding_box = Aabb2d::new(
+            transform.translation.truncate(),
+            transform.scale.truncate() / 2.,
+        );
 
         if player_bounding_box.intersects(&obstacle_bounding_box) {
             events.send_default();
@@ -315,12 +352,15 @@ fn display_game_over_text(
                     TextLayout::new_with_justify(JustifyText::Center),
                     TextFont::from_font_size(48.0),
                     GameOverText,
+                    InstructionsText,
                     Transform::from_xyz(
                         0.0,
                         0.0 + (window.width() / 2.) - (window.width() / 4.),
                         0.0,
                     ),
                 ));
+
+                add_instructions_text(&mut commands, &window)
             }
 
             _ => {}
@@ -328,21 +368,32 @@ fn display_game_over_text(
     }
 }
 
-fn hide_game_over_text(
+fn add_instructions_text(commands: &mut Commands, window: &Window) {
+    commands.spawn((
+        Text2d::new("Hit Space to Play"),
+        TextLayout::new_with_justify(JustifyText::Center),
+        TextFont::from_font_size(48.0),
+        HitSpaceText,
+        InstructionsText,
+        Transform::from_xyz(0.0, 0.0 - (window.width() / 4.), 0.0),
+    ));
+}
+
+fn maybe_hide_instructions_text(
     mut commands: Commands,
-    game_over_text_query: Query<Entity, With<GameOverText>>,
+    ui_elements_query: Query<Entity, With<InstructionsText>>,
     mut game_state_events: EventReader<GameStateEvent>,
 ) {
-    if game_over_text_query.is_empty() {
+    if ui_elements_query.is_empty() {
         return;
     }
 
-    let game_over_text_entity = game_over_text_query.single();
-
     for event in game_state_events.read().into_iter() {
-        match event.from {
-            GameStates::GameOver => {
-                commands.entity(game_over_text_entity).despawn();
+        match event.to {
+            GameStates::Play => {
+                for entity in ui_elements_query.iter() {
+                    commands.entity(entity).despawn();
+                }
             }
 
             _ => {}
