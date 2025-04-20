46c591886005ebad438f6e4e6632f5f72126ff58
Maxime Buffa <mbuffa@users.noreply.github.com>
2025-04-20 16:00:39 +0200

    Fix collisions and hardcoded values

--- examples/006-xp-grab.rs
+++ examples/006-xp-grab.rs
@@ -6,9 +6,13 @@ use bevy::prelude::*;
 
 const GOLD: Srgba = bevy::color::palettes::css::GOLD;
 const GREEN: Srgba = bevy::color::palettes::css::GREEN;
+const WHITE: Srgba = bevy::color::palettes::css::BLACK;
 
 const PLAYER_SPEED: f32 = 100.0;
 const XP_SPEED: f32 = 20.0;
+
+const PLAYER_SIZE: f32 = 32.0;
+const COLLIDER_SIZE: f32 = 96.0;
 
 #[derive(Component)]
 struct XP;
@@ -17,21 +21,10 @@ struct XP;
 struct Player;
 
 #[derive(Component)]
-struct PlayerCollider;
+struct PlayerCollider(f32);
 
 #[derive(Resource)]
 struct Experience(u128);
-
-enum Axises {
-    Horizontal,
-    Vertical,
-}
-
-#[derive(Component)]
-struct Gauge {
-    axis: Axises,
-    value: u8,
-}
 
 #[derive(Component)]
 struct XpGauge;
@@ -50,8 +43,8 @@ fn main() {
             (
                 move_player,
                 maybe_spawn_xp,
-                detect_collider_collision,
-                detect_player_collision,
+                detect_xp_collider_collision,
+                detect_xp_player_collision,
                 maybe_increase_score,
             ),
         )
@@ -66,8 +59,6 @@ fn setup(
 ) {
     let window: &Window = window_q.into_inner();
 
-    println!("{:?}", window.width());
-
     commands.spawn(Camera2d);
 
     commands.spawn((
@@ -80,25 +71,28 @@ fn setup(
         },
         BackgroundColor(Color::from(GOLD)),
         XpGauge,
-        Gauge {
-            axis: Axises::Horizontal,
-            value: 0,
-        },
         Transform::from_xyz(0.0, 0.0 + 48.0 + 16.0, 0.0),
     ));
 
     commands
         .spawn((
             Player,
-            Transform::from_xyz(0.0, 0.0, 0.0),
-            Mesh2d(meshes.add(Rectangle::new(32.0, 32.0))),
-            MeshMaterial2d(materials.add(Color::srgb(1.0, 1.0, 1.0))),
+            Transform {
+                translation: Vec3::new(0.0, 0.0, 0.0),
+                scale: Vec3::new(PLAYER_SIZE, PLAYER_SIZE, 1.0),
+                ..default()
+            },
+            Mesh2d(meshes.add(Rectangle::new(1.0, 1.0))),
+            MeshMaterial2d(materials.add(Color::from(WHITE))),
         ))
         .with_children(|parent: &mut ChildBuilder<'_>| {
             parent.spawn((
-                PlayerCollider,
-                Transform::from_xyz(0.0, 0.0, -1.0),
-                Mesh2d(meshes.add(Circle::new(96.0))),
+                PlayerCollider(COLLIDER_SIZE),
+                Transform {
+                    translation: Vec3::new(0.0, 0.0, -1.0),
+                    ..default()
+                },
+                Mesh2d(meshes.add(Circle::new(COLLIDER_SIZE / PLAYER_SIZE))),
                 MeshMaterial2d(materials.add(Color::from(GREEN.with_alpha(0.1)))),
             ));
         });
@@ -154,27 +148,20 @@ fn do_spawn_xp(
     ));
 }
 
-// FIXME: Query collider radius
-fn detect_collider_collision(
-    // collider_q: Single<&Transform, With<PlayerCollider>>,
+fn detect_xp_collider_collision(
+    collider_q: Single<&PlayerCollider>,
     player_q: Single<&Transform, With<Player>>,
     mut xp_q: Query<&mut Transform, (With<XP>, Without<PlayerCollider>, Without<Player>)>,
     time: Res<Time>,
 ) {
     let elapsed = time.delta_secs();
 
-    // let collider_t = collider_q.into_inner();
+    let collider = collider_q.into_inner();
     let player_t = player_q.into_inner();
 
-    let bounding_circle = BoundingCircle::new(player_t.translation.truncate(), 96.0);
+    let bounding_circle = BoundingCircle::new(player_t.translation.truncate(), collider.0);
 
     for mut xp_t in xp_q.iter_mut() {
-        // println!(
-        //     "{:?} {:?}",
-        //     player_t.translation.truncate(),
-        //     xp_t.translation.truncate()
-        // );
-
         if do_detect_cllision(
             bounding_circle,
             Aabb2d::new(xp_t.translation.truncate(), xp_t.scale.truncate() / 2.0),
@@ -203,20 +190,25 @@ fn do_detect_cllision(bounding_circle: BoundingCircle, aabb: Aabb2d) -> bool {
     return false;
 }
 
-fn detect_player_collision(
+fn detect_xp_player_collision(
     mut commands: Commands,
     player_q: Single<&Transform, With<Player>>,
     xp_q: Query<(Entity, &Transform), With<XP>>,
     mut events: EventWriter<CollisionEvent>,
 ) {
     let player_t = player_q.into_inner();
-    let player_aabb = Aabb2d::new(player_t.translation.truncate(), player_t.scale.truncate());
+
+    let player_aabb = Aabb2d::new(
+        player_t.translation.truncate(),
+        player_t.scale.truncate() / 2.0,
+    );
 
     for (xp_entity, xp_t) in xp_q.iter() {
         let xp_aabb = Aabb2d::new(xp_t.translation.truncate(), xp_t.scale.truncate() / 2.0);
 
-        if player_aabb.intersects(&xp_aabb) {
+        if player_aabb.intersects(&xp_aabb.bounding_circle()) {
             events.send(CollisionEvent);
+
             commands.entity(xp_entity).despawn();
         }
     }
@@ -233,9 +225,13 @@ fn maybe_increase_score(
 
     let mut gauge_node = gauge_q.into_inner();
 
+    let max_width: f32 = match gauge_node.max_width {
+        Val::Px(max_width) => max_width,
+        _ => 0.0,
+    };
+
     for _event in events.read() {
         experience.0 += 100;
-        // FIXME: Find how to get the max width on the node as a f32!
-        gauge_node.width = Val::Px(experience.0 as f32 / 1000.0) * 1280.0;
+        gauge_node.width = Val::Px(experience.0 as f32 / 1000.0) * max_width;
     }
 }
